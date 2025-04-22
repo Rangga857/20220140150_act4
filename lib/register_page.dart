7d3447cc6191b59574ba8beee809f03407190f57
Rangga857 <azharrangga28@gmail.com>
2025-04-22 22:05:52 +0700

    create form register dan validasi input

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -10,8 +10,65 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Form(child: Padding(padding: 
+          const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Register',
+              ),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value){
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value){
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Register'),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Navigator.pushNamed(context, '/login');
+                  Navigator.pop(context);
+                  // Navigator.push(
+                  //   context, 
+                  //   MaterialPageRoute(
+                  //     builder: (context) => const LoginPage(),
+                  //   ),
+                  // );
+                },
+                child: const Text('Sudah punya akun? Login sekarang!'),
+              ),
+            ],
+          ),
+        )
+      ),
+    );
   }
 }
