1b46a6bce99ff58412bd8c3b252cd11459067036
Rangga857 <azharrangga28@gmail.com>
2025-04-22 22:51:07 +0700

    Add parameters untuk DetailOrderPage

--- lib/orderdetail_page.dart
+++ lib/orderdetail_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class DetailOrderPage extends StatelessWidget {
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+  const DetailOrderPage(
+    {super.key,
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+    });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+    );
   }
 }
